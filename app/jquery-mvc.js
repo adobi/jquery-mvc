7056ae9b8c9b8afc3a674c63ad9d1e7a8e5655a4
Attila Dobi <hello@adobi.hu>
2012-05-15 16:31:57 +0200

    .

--- app/jquery-mvc.js
+++ app/jquery-mvc.js
@@ -5,7 +5,9 @@
 			App.Router = App.Router || {},
       App.Utils = App.Utils || {},
       App.Helpers = App.Helpers || {}
-
+  
+  //var modules
+      
 	head.js('app/core/router.js', 'app/core/model.js', 'app/controllers/list.js', 'app/controllers/contact.js', 'app/core/utils.js', function() {
 	    
   	  App.Utils.onLoad()
